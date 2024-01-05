b4dfc9420d58775cc9d129f977c1b98df45325de
keziahNsoh <porepeya@gmail.com>
2024-01-04 19:04:34 -0800

    solution five

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -2,6 +2,6 @@
 if __name__ == "__main__":
     from hidden_4 import *
     allf = dir()
-    for i in range (0, len(allf)):
+    for i in range(0, len(allf)):
         if allf[i][:2] != "__":
             print("{:s}".format(allf[i]))
