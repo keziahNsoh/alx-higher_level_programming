be2987ae24aefa0282e0bbfd7b353cd603a9deab
keziahNsoh <porepeya@gmail.com>
2024-01-09 04:02:52 -0800

    solution five

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
+    temp = my_list[:]
     if idx < 0:
-        return (my_list)
+        return temp
     if idx > len(my_list) - 1:
-        return (my_list)
-    my_list[idx] = element
-    return (my_list)
+        return temp
+    temp[idx] = element
+    return temp
