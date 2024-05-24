8ff554f39acfdd5aff1d96124827d075966b4d77
keziahNsoh <porepeya@gmail.com>
2024-05-23 21:21:58 -0700

    14-average.sql 13-change_class.sql

--- 0x0D-SQL_introduction/13-change_class.sql
+++ 0x0D-SQL_introduction/13-change_class.sql
@@ -1,3 +1,5 @@
--- Remove all records with a score <= 5 in second_table
-DELETE FROM hbtn_0c_0.second_table WHERE score <= 5;
+-- TASK: Remove all records with a score <= 5 in the table second_table
+
+DELETE FROM second_table
+WHERE score <= 5;
 
--- 0x0D-SQL_introduction/14-average.sql
+++ 0x0D-SQL_introduction/14-average.sql
@@ -1,3 +1,5 @@
--- Compute the score average of all records in second_table
-SELECT AVG(score) AS average FROM hbtn_0c_0.second_table;
+-- TASK: Compute the score average of all records in the table second_table
+
+SELECT AVG(score) AS average
+FROM second_table;
 
