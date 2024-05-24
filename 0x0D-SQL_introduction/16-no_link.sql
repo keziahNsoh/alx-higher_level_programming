2bb22ff23a51717f60bf87ed4de4392ebb7b3305
keziahNsoh <porepeya@gmail.com>
2024-05-23 21:12:07 -0700

    16-no_link.sql

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,6 +1,7 @@
 -- List all records of second_table with a name value, ordered by descending score
+
 SELECT score, name
-FROM hbtn_0c_0.second_table
+FROM second_table
 WHERE name IS NOT NULL
 ORDER BY score DESC;
 
