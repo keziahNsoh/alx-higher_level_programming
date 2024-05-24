e73f51deb977c8db6e886c46bbdec16baac8e3b1
keziahNsoh <porepeya@gmail.com>
2024-05-23 21:15:36 -0700

    15-groups.sql

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,3 +1,7 @@
--- List the number of records with the same score in second_table
-SELECT score, COUNT(*) AS number FROM hbtn_0c_0.second_table GROUP BY score ORDER BY number DESC;
+-- TASK: List the number of records with the same score in the table second_table
+
+SELECT score, COUNT(*) AS number
+FROM second_table
+GROUP BY score
+ORDER BY number DESC;
 
