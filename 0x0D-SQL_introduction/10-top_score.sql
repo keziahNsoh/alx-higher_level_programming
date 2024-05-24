6bb977f659e40b674fc58df64660f008bfd46b9b
keziahNsoh <porepeya@gmail.com>
2024-05-23 21:41:38 -0700

    10-top_score.sql

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,5 +1,13 @@
 -- List all records of second_table ordered by score (top first)
-SELECT score, name
-FROM hbtn_0c_0.second_table
-ORDER BY score DESC;
+CREATE TABLE IF NOT EXISTS hbtn_0c_0.second_table (
+    id INT,
+    name VARCHAR(256),
+    score INT
+);
+
+INSERT INTO hbtn_0c_0.second_table (id, name, score) VALUES
+(1, 'John', 10),
+(2, 'Alex', 3),
+(3, 'Bob', 14),
+(4, 'George', 8);
 
