c08875f34ce53d80de034eab667400b76cd9fb6c
keziahNsoh <porepeya@gmail.com>
2024-05-28 17:30:39 -0700

    Integers addition

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -14,6 +14,7 @@ def add_integer(a, b=98):
     Returns:
         int: The addition of a and b.
     """
+
     # Check if a and b are integers or floats
     if not isinstance(a, (int, float)) or not isinstance(b, (int, float)):
         raise TypeError("a must be an integer or b must be an integer")
@@ -27,37 +28,6 @@ def add_integer(a, b=98):
     # Return the addition of a and b
     return a + b
 
-if __name__ == "__main__":
-    print(add_integer(5, 3))
-
-#!/usr/bin/python3
-
-def add_integer(a, b=98):
-    """
-    Adds two integers.
-
-    Args:
-        a (int or float): The first integer.
-        b (int or float, optional): The second integer. Defaults to 98.
-
-    Raises:
-        TypeError: If a or b is not an integer or float.
-
-    Returns:
-        int: The addition of a and b.
-    """
-    # Check if a and b are integers or floats
-    if not isinstance(a, (int, float)) or not isinstance(b, (int, float)):
-        raise TypeError("a must be an integer or b must be an integer")
-
-    # Cast a and b to integers if they are floats
-    if isinstance(a, float):
-        a = int(a)
-    if isinstance(b, float):
-        b = int(b)
-
-    # Return the addition of a and b
-    return a + b
 
 if __name__ == "__main__":
     print(add_integer(5, 3))
