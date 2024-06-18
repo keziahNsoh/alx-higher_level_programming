e29a85cb4add3175dd2d600cc60a8522f7370330
keziahNsoh <porepeya@gmail.com>
2024-06-18 09:29:13 -0700

    7-multi_c.js

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,15 +1,11 @@
 #!/usr/bin/node
-const args = process.argv.slice(2);
-const x = parseInt(args[0]);
 
-if (isNaN(x))
-{
-	console.log("Missing number of occurrences");
-}
-else
-{
-	for (let i = 0; i < x; i++)
-	{
-		console.log("C is fun");
-	}
+const num = parseInt(process.argv[2]);
+
+if (isNaN(num)) {
+  console.log('Missing number of occurrences');
+} else {
+  for (let i = 0; i < num; i++) {
+    console.log('C is fun');
+  }
 }
