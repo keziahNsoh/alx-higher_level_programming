93f5d6eb33c441b0fc91e40042dfc045c1d0952d
keziahNsoh <porepeya@gmail.com>
2024-06-18 09:18:05 -0700

    6-multi_languages_loop.js

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,13 +1,7 @@
 #!/usr/bin/node
 
 const lines = ["C is fun", "Python is cool", "JavaScript is amazing"];
-let output = '';
 
 for (let i = 0; i < lines.length; i++) {
-  output += lines[i];
-  if (i !== lines.length - 1) {
-    output += '\n';
-  }
+  console.log(lines[i]);
 }
-
-console.log(output);
