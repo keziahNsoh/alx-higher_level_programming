26bd8be0c9e051eeadfa887a23693607c9f353a0
keziahNsoh <porepeya@gmail.com>
2024-06-18 09:57:18 -0700

    9-add.js

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -3,9 +3,8 @@ const args = process.argv.slice(2);
 const a = parseInt(args[0]);
 const b = parseInt(args[1]);
 
-function add(a, b)
-{
-	return a + b;
+function add(a, b) {
+  return a + b;
 }
 
 console.log(add(a, b));
