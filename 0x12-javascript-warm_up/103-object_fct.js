c44618cc8c1f2e2c4486aeb21177f8d06554d635
keziahNsoh <porepeya@gmail.com>
2024-06-18 05:35:10 -0700

    103-object_fct.js

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -1,20 +1,16 @@
 #!/usr/bin/node
-const myObject =
-	{
-		type: 'object',
-		value: 12,
-		incr: function()
-		{
-			this.value++;
-		}
-	};
-console.log(myObject);
+const myObject = {
+  type: 'object',
+  value: 12,
+  incr: function() {
+    this.value++;
+  }
+};
 
+console.log(myObject);
 myObject.incr();
 console.log(myObject);
-
 myObject.incr();
 console.log(myObject);
-
 myObject.incr();
 console.log(myObject);
