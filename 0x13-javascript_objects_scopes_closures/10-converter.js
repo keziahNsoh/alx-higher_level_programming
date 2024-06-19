a3dd2d782571522409f1f475717ce7de088cc0e0
keziahNsoh <porepeya@gmail.com>
2024-06-19 05:40:25 -0700

    some solutions

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -3,9 +3,7 @@
 exports.converter = function(base) {
   // Recursive function to convert number to specified base
   return function convert(num) {
-    if (num >= base) {
-      convert((num / base) | 0);
-    }
+    num >= base && convert(num / base | 0);
     process.stdout.write((num % base).toString(base));
   };
 };
