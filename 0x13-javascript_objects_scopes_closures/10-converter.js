5a615357703199765cea40660946885f8414964c
keziahNsoh <porepeya@gmail.com>
2024-06-19 05:37:22 -0700

    some solutions

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -2,12 +2,10 @@
 
 exports.converter = function(base) {
   // Recursive function to convert number to specified base
-  function convert(num) {
+  return function convert(num) {
     if (num >= base) {
       convert((num / base) | 0);
     }
-    process.stdout.write((num % base).toString());
-  }
-
-  return convert;
+    process.stdout.write((num % base).toString(base));
+  };
 };
