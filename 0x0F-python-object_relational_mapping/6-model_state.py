1b4da4d0c5601c09390150fb5a52510dfbc7bda3
keziahNsoh <porepeya@gmail.com>
2024-06-22 04:53:00 -0700

    First state model

--- 0x0F-python-object_relational_mapping/6-model_state.py
+++ 0x0F-python-object_relational_mapping/6-model_state.py
@@ -1,11 +1,19 @@
 #!/usr/bin/python3
-"""Start link class to table in database 
+"""
+Script that creates the State class and Base instance
 """
 import sys
 from model_state import Base, State
-
-from sqlalchemy import (create_engine)
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    # Create engine to connect to MySQL database
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+
+    # Create all tables in the engine
     Base.metadata.create_all(engine)
+
+
--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,12 +1,20 @@
+# model_state.py
+"""
+Define the State class and Base instance.
+"""
+
 from sqlalchemy import Column, Integer, String
-from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
 
+# Create a declarative base instance
 Base = declarative_base()
 
+# Define the State class
 class State(Base):
+    """
+    State class represents a state with id and name.
+    """
     __tablename__ = 'states'
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
     name = Column(String(128), nullable=False)
-    cities = relationship("City", back_populates="state")  # Establishes bidirectional relationship
 
