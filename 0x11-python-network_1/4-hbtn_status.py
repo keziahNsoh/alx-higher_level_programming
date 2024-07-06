a398b59bb5d61ca6f4b980f32dad7cfaa9818fe2
keziahNsoh <porepeya@gmail.com>
2024-07-06 05:01:05 +0000

    Add requests

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+
+"""
+Response header value
+Description: A Python script that fetches https://alx-intranet.hbtn.io/status with requests
+"""
+
+import requests
+
+def main():
+   result = requests.get("https://alx-intranet.hbtn.io/status")
+   response_content = result.text
+   response_type = type(response_content)
+   print("Body response:\n\t- type: {}\n\t- content: {}".format(response_type, response_content))
+
+
+
+
+
+
+
+if __name__ == "__main__":
+   main()
+
