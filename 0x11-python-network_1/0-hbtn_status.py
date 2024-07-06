a652846cb8ec54896592bf9f9315b3e165c4a60c
keziahNsoh <porepeya@gmail.com>
2024-07-06 04:43:11 +0000

    change utf8

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -11,7 +11,7 @@ def main():
      response_body = result.read()
      response_type = type(response_body)
      response_content = response_body
-     utf8_content = response_body.decode('utf8')
+     utf8_content = response_body.decode('utf-8')
 
      #Display of the body of the response,response_type, response_content and response_type, response_content
 
